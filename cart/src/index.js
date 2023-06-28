7cb4c1c0b97cadf6a1729f240e896f61b8bc6a9c
Wesley de Paula <wesleyop@hotmail.com>
2023-06-28 10:54:30 -0300

    Using Shared Modules

--- cart/src/index.js
+++ cart/src/index.js
@@ -1,5 +1,1 @@
-import faker from 'faker';
-
-const cartText = `<div>You have ${faker.random.number()} items in yout cart</div>`;
-
-document.querySelector('#dev-cart').innerHTML = cartText;
+import('./bootstrap');
